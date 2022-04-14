962f23090dc61aa3919de02d4a695ebbc67ac33d
RicardoGBrito <Ricardobrito_348@hotmail.com>
2022-04-13 22:50:28 -0300

    Implementada a funcao de tarefa

--- scripts/utils.js
+++ scripts/utils.js
@@ -8,9 +8,14 @@ window.tag = (elementoTag) =>{
     return document.querySelector(elementoTag);
 }
 
-window.criandoElemento = (tag, classe) =>{
+window.tagClasses = (tagClass) => {
+    return document.getElementsByClassName(tagClass);
+}
+
+window.criandoElemento = (tag, classe,id) =>{
     let elemento = document.createElement(tag);
     elemento.classList.add(classe);
+    elemento.setAttribute('id',id)
     
     return elemento;
 }
@@ -31,25 +36,66 @@ window.dadosApi = (caminho, metodo, corpo, jwt) => {
     return fetch(window.enderecoApi+caminho,options).then(response => response.json())
 }
 
-window.addTarefa = (descricao, criacao) => {
-    let div = window.inputs('skeleton');
-    let li = window.criandoElemento('li', "tarefa")
+window.addTarefa = (identificadorTag, descricao, criacao,id) => {
+    let div = window.tag(identificadorTag);
+    let li = window.criandoElemento('li', "tarefa", parseInt(id))
     let div1 = window.criandoElemento('div', "not-done")
     let div2 = window.criandoElemento('div', "descricao")
     let p1 = window.criandoElemento('p', "nome")
     let p2 = window.criandoElemento('p', "timestamp")
+    let p3 = window.criandoElemento('p',"hide")
+    
 
     let node1 = document.createTextNode(descricao);
-    let node2 = document.createTextNode(criacao);
+    let node2 = document.createTextNode(window.tratarTimeStamp(criacao));
+    let node3 = document.createTextNode(id);
 
+    console.log(node1)
     p1.appendChild(node1);
     p2.appendChild(node2);
-
+    p3.appendChild(node3);
+    console.log(p2)
+    console.log(p3)
     li.appendChild(div1);
     li.appendChild(div2);
     div2.appendChild(p1);
     div2.appendChild(p2);
-
+    div2.appendChild(p3);
+    
     div.appendChild(li);
     
+    div1.addEventListener('click', (evento)=>{
+        
+        let divDescricao = evento.target.nextElementSibling;
+
+        tarefaUpdate(divDescricao.firstChild.innerText, divDescricao.lastChild.innerText);
+    })
+    
 }
+
+window.apagarTarefa = (id) =>{
+    let elemento = document.getElementById(id);
+    elemento.remove();
+
+};
+
+window.tratarTimeStamp = (timeStamp) => {
+    let data1 = timeStamp.split('T');
+    let data2 = data1[0].split('-');
+    
+    return `Criada em: ${data2[2]}/${data2[1]}/${data2[0][2]+data2[0][3]}`
+
+};
+
+
+function tarefaUpdate(descricao, id){
+    let objBody = {
+        description:descricao.innerText,
+        completed:true
+    }
+
+    window.dadosApi(`tasks/${id}`,'PUT', objBody,jwt).then(dados => {
+        window.addTarefa('.tarefas-terminadas', dados.description, dados.createdAt, dados.id)
+        window.apagarTarefa(dados.id);
+    })
+};
